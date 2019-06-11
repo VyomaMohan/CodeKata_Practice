23cc53d63a0864f896328f9b1187a8a868fccab4
VyomaMohan <46680968+VyomaMohan@users.noreply.github.com>
2019-06-11 10:53:18 +0530

    Update codekata_erasthonesprime.c

--- codekata_erasthonesprime.c
+++ codekata_erasthonesprime.c
@@ -9,22 +9,26 @@ main(){
 	//if(low==1)
 	//arr[0]=1;
 	
-	for(i=0;i<limit;i++){
-		num=i+low;
+	for(i=2;i<=high;i++){
+		num=i;
 		increment=num;
-		if(arr[i]==0&&num!=1){
-			count++;
+		if(arr[i]==0){
 			num=num+num;
 			while(num<=high){
-				arr[num-low]=1;
+				arr[num]=1;
 				num=num+increment;
 			}
 		}
 	}
 	
+	for(i=low;i<=high;i++){
+		if(arr[i]==0){
+			count++;
+		}
+	}
+	
 	//for(i=0;i<limit;i++)
 	//printf("%d:%d ",low+i,arr[i]);
-	
 	//printf("\n");
 	
 	printf("%d",count);
